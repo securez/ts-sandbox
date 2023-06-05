af2d0f76698e8f1a10c35519ff017abd951b9ef2
Marcos Lois Bermúdez <marcos.lois@gmail.com>
2023-06-05 16:20:19 +0200

    feat: more tree queries

--- src/test-tree.ts
+++ src/test-tree.ts
@@ -27,7 +27,7 @@ AppDataSource.initialize()
     // Select a zone and all of its ancestors
     const zoneWithAncestors = await repository.find({
       relations: {
-        descendants: true
+        descendants: true,
       },
       where: {
         id: 4,
@@ -39,50 +39,51 @@ AppDataSource.initialize()
     );
 
     // Get all roots and its decendands order by name
-    /*
-    SELECT
-        z.*,
-        zt.length 
-    FROM
-        zones z
-    JOIN 
-        zones_tree zt ON
-        z.zone_id = zt.descendant_id
-    JOIN zones rt ON
-        rt.zone_id = zt.ancestor_id
-    WHERE
-        rt.parent_zone_id IS NULL
-    ORDER BY
-        rt.name ASC,
-        zt.length ASC,
-        z.name ASC
-    ;
-    */
-    const zoneWithDescendantOrder = await repository
-      .createQueryBuilder("zone")
-      .select(["zone", "zt"])
-      .innerJoin("zone.descendants", "zt")
-      .innerJoin("zt.ancestor", "rt")
-      .where("rt.parent_zone_id IS NULL")
-      .orderBy("rt.name", "ASC")
-      .addOrderBy("zt.length", "ASC")
-      .addOrderBy("zone.parentId", "ASC")
-      .addOrderBy("zone.name", "ASC")
-      .getMany();
+    const zoneWithDescendantOrder = (
+      await repository
+        .createQueryBuilder("zone")
+        .select(["zone", "zt"])
+        .innerJoin("zone.descendants", "zt")
+        .innerJoin("zt.ancestor", "rt")
+        .where("rt.parent_zone_id IS NULL")
+        .orderBy("rt.name", "ASC")
+        .addOrderBy("zt.length", "ASC")
+        .addOrderBy("zone.parentId", "ASC")
+        .addOrderBy("zone.name", "ASC")
+        .getMany()
+    ).map((zone) => {
+      return {
+        id: zone.id,
+        parentId: zone.parentId,
+        name: zone.name,
+        depth: zone.descendants[0].length,
+      };
+    });
 
     console.log(
       "Descendants Order:  " +
         JSON.stringify(zoneWithDescendantOrder, undefined, 2)
     );
 
-    // Select node and all descendants in order
-    const nodeWithDescendantOrder = await repository
-      .createQueryBuilder("zone")
-      .select(["zone", "zt"])
-      .innerJoin("zone.ancestors", "zt")
-      .where("zt.descendant_id = 3")
-      .orderBy("zt.length", "ASC")
-      .getMany();
+    // Select node 3 and all descendants in order
+    const nodeWithDescendantOrder = (
+      await repository
+        .createQueryBuilder("zone")
+        .select(["zone", "zt"])
+        .innerJoin("zone.ancestors", "zt")
+        .where("zt.descendant_id = 3")
+        .orderBy("zt.length", "ASC")
+        .addOrderBy("zt.length", "ASC")
+        .addOrderBy("zone.name", "ASC")
+        .getMany()
+    ).map((zone) => {
+      return {
+        id: zone.id,
+        parentId: zone.parentId,
+        name: zone.name,
+        depth: zone.ancestors[0].length,
+      };
+    });
 
     console.log(
       "Descendants Order:  " +
